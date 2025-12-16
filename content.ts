0bfe2fea89ce18aa118307730006fc39a62615ce
Chase Hartsell <cmhartsell@me.com>
2025-12-15 22:49:26 -0800

    Display dashboard per project
    - Parse URL into project, repo, branch, path
    - Display dashboard on project page

--- content.ts
+++ content.ts
@@ -2,6 +2,13 @@ const STORAGE_KEY = "chromium-cs-stars";
 let currentPath = "";
 let isInjectingDashboard = false;
 
+interface Path {
+  project: string;
+  repo?: string;
+  branch?: string;
+  path?: string;
+}
+
 function init() {
   const observer = new MutationObserver(() => {
     handlePageChange();
@@ -13,17 +20,49 @@ function init() {
 
 function handlePageChange() {
   const url = window.location.href;
-  const isLanding = url.endsWith("source.chromium.org/");
+  const path = parseUrl(url);
+  if (!path) return;
 
-  if (isLanding) {
-    injectDashboard();
+  if (path.project && !path.repo && !path.branch && !path.path) {
+    injectDashboard(path.project);
+  } else if (path.branch && path.path) {
+    hideDashboard(path.project);
+    injectStarButton(path);
   } else {
-    hideDashboard();
-    injectStarButton();
+    hideDashboard(path.project);
   }
 }
 
-function injectStarButton() {
+function parseUrl(url: string): Path | null {
+  const pattern =
+    /^(?:https?:\/\/)?source\.chromium\.org\/(?<project>[^\/]+)(?:\/(?<repo>.+?)(?:\/\+\/(?<branch>[^:]+):(?<path>[^;]*))?)?\/?(?=[?;]|$)/;
+
+  const match = url.match(pattern);
+  if (!match || !match.groups) return null;
+
+  let project = decodeURIComponent(match.groups.project);
+  let repo =
+    match.groups.repo !== undefined
+      ? decodeURIComponent(match.groups.repo)
+      : undefined;
+  let branch =
+    match.groups.branch !== undefined
+      ? decodeURIComponent(match.groups.branch)
+      : undefined;
+  let path =
+    match.groups.path !== undefined
+      ? decodeURIComponent(match.groups.path)
+      : undefined;
+
+  return {
+    project,
+    repo,
+    branch,
+    path,
+  };
+}
+
+function injectStarButton(currentPath: Path) {
   if (document.querySelector(".ccss-star-btn")) return;
 
   // Try to find the breadcrumb container.
@@ -40,14 +79,10 @@ function injectStarButton() {
       await removeStar(currentPath);
       updateBtnVisuals(btn, /*isStarred=*/ false);
     } else {
-      const name = document.title.replace(" - Chromium Code Search", "");
-      await addStar(currentPath, name);
+      await addStar(currentPath);
       updateBtnVisuals(btn, /*isStarred=*/ true);
     }
   };
-
-  // Determine current path from URL.
-  currentPath = window.location.pathname + window.location.search;
 
   // Check state.
   checkIsStarred(currentPath).then((isStarred) => {
@@ -58,7 +93,7 @@ function injectStarButton() {
   breadcrumbContainer.prepend(btn);
 }
 
-function updateBtnVisuals(btn, isStarred) {
+function updateBtnVisuals(btn: HTMLButtonElement, isStarred: boolean) {
   if (isStarred) {
     btn.classList.add("starred");
     btn.innerHTML = "★";
@@ -68,25 +103,28 @@ function updateBtnVisuals(btn, isStarred) {
   }
 }
 
-async function injectDashboard() {
+async function injectDashboard(project: string) {
   // Prevent multiple attempts to inject the dashboard.
   if (isInjectingDashboard) return;
   isInjectingDashboard = true;
 
-  const maybeDashboard = document.querySelector(".ccss-dashboard");
+  const maybeDashboard = document.querySelector(
+    `#${project}-dashboard.ccss-dashboard`
+  );
   if (maybeDashboard) {
     maybeDashboard.classList.remove("hidden");
     isInjectingDashboard = false;
     return;
   }
 
-  const targetContainer = document.querySelector("whitelabelled-search");
+  const targetContainer = document.querySelector(".project-card");
   if (!targetContainer) {
     isInjectingDashboard = false;
     return;
   }
 
   const dashboard = document.createElement("div");
+  dashboard.id = `${project}-dashboard`;
   dashboard.className = "ccss-dashboard";
 
   const title = document.createElement("h3");
@@ -102,24 +140,31 @@ async function injectDashboard() {
   dashboard.appendChild(list);
 
   // Populate list with files.
-  const data = await getStarredPaths();
+  const stars = (await getStarredPaths()).filter(
+    (star) => star.project === project
+  );
   const NO_FILES_INNER_HTML = "<li>No starred files or folders yet.</li>";
-  if (data.length === 0) {
+  if (stars.length === 0) {
     list.innerHTML = NO_FILES_INNER_HTML;
   } else {
-    data.forEach((item) => {
+    stars.forEach((star) => {
       const li = document.createElement("li");
-
       const link = document.createElement("a");
-      link.href = `https://source.chromium.org${item.path}`;
-      link.innerText = item.name || item.path;
+
+      link.href = `https://source.chromium.org/${star.project}/${star.repo}`;
+      if (star.branch) link.href += `/+/${star.branch}:`;
+      if (star.path) link.href += star.path;
+
+      link.innerText = "";
+      if (star.repo) link.innerText += star.repo;
+      if (star.path) link.innerText += ":" + star.path;
 
       const removeBtn = document.createElement("span");
       removeBtn.className = "ccss-remove";
       removeBtn.innerText = "[Remove]";
       removeBtn.onclick = async () => {
-        await removeStar(item.path);
-        let isLastElement = li.parentElement.children.length === 1;
+        await removeStar(star);
+        let isLastElement = li.parentElement!.children.length === 1;
         li.remove();
         if (isLastElement) {
           list.innerHTML = NO_FILES_INNER_HTML;
@@ -132,42 +177,49 @@ async function injectDashboard() {
     });
   }
 
-  targetContainer.appendChild(dashboard, targetContainer.firstChild);
+  targetContainer.appendChild(dashboard);
 
   isInjectingDashboard = false;
 }
 
-function hideDashboard() {
-  const maybeDashboard = document.querySelector(".ccss-dashboard");
+function hideDashboard(project: string) {
+  const maybeDashboard = document.querySelector(
+    `#${project}-dashboard.ccss-dashboard`
+  );
   if (maybeDashboard) maybeDashboard.classList.add("hidden");
 }
 
-async function getStarredPaths() {
+async function getStarredPaths(): Promise<Path[]> {
   const storage = await chrome.storage.sync.get(STORAGE_KEY);
-  return storage[STORAGE_KEY] || [];
+  return (storage[STORAGE_KEY] || []) as Path[];
 }
 
-async function addStar(path, name) {
+async function addStar(path: Path) {
   const stars = await getStarredPaths();
-  if (!stars.find((s) => s.path === path)) {
-    stars.unshift({
-      path,
-      name,
-      date: Date.now(),
-    });
+  if (!stars.find((s) => arePathsEqual(s, path))) {
+    stars.unshift(path);
     chrome.storage.sync.set({ [STORAGE_KEY]: stars });
   }
 }
 
-async function removeStar(path) {
+async function removeStar(path: Path) {
   let stars = await getStarredPaths();
-  stars = stars.filter((s) => s.path !== path);
+  stars = stars.filter((s) => !arePathsEqual(s, path));
   chrome.storage.sync.set({ [STORAGE_KEY]: stars });
 }
 
-async function checkIsStarred(path) {
+async function checkIsStarred(path: Path) {
   const stars = await getStarredPaths();
-  return !!stars.find((s) => s.path === path);
+  return !!stars.find((s) => arePathsEqual(s, path));
+}
+
+function arePathsEqual(a: Path, b: Path) {
+  return (
+    a.project === b.project &&
+    a.repo === b.repo &&
+    a.branch === b.branch &&
+    a.path === b.path
+  );
 }
 
 init();
